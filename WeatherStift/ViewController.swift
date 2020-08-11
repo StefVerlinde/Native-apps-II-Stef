27029b4e2b182c9092cb44edf1be073b7e1e689b
Stef Verlinde <stef.verlinde@student.hogent.be>
2020-08-11 15:52:44 +0200

    First commit, playing around with UI

---
+++ WeatherStift/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  WeatherStift
+//
+//  Created by Stef Verlinde on 11/08/2020.
+//  Copyright © 2020 Stef Verlinde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var condLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func addLocationButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func locationButtonTapped(_ sender: Any) {
+        
+    }
+}
+
