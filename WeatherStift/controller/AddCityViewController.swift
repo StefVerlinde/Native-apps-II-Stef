c3dae8bb269662c9caa82d214d381a8ed62e7a45
Stef Verlinde <stef.verlinde@student.hogent.be>
2020-08-12 17:56:10 +0200

    Adding ui for adding cities + adding new view controller

---
+++ WeatherStift/controller/AddCityViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddCityViewController.swift
+//  WeatherStift
+//
+//  Created by Stef Verlinde on 12/08/2020.
+//  Copyright © 2020 Stef Verlinde. All rights reserved.
+//
+
+import UIKit
+
+class AddCityViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    private func setupViews(){
+        view.backgroundColor = UIColor.init(white: 0.3, alpha: 0.4)
+    }
+}
