9a429a23b91f479c0b5fc4cd2c2a8ddb314074b0
Stef Verlinde <stef.verlinde@student.hogent.be>
2020-08-14 16:42:13 +0200

    When user clicks next to cityadd popup, remove view

--- WeatherStift/controller/AddCityViewController.swift
+++ WeatherStift/controller/AddCityViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class AddCityViewController: UIViewController {
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        setupGestures()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cityTextField.becomeFirstResponder()
     }
     
     private func setupViews(){
         view.backgroundColor = UIColor.init(white: 0.3, alpha: 0.4)
+        statusLabel.isHidden = true
+    }
+    
+    private func setupGestures() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissCityViewController))
+        //This + extension methode removes error when user touches on addCityView
+        tapGesture.delegate = self
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func dismissCityViewController() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func searchButtonTapped(_ sender: Any) {
+        
+    }
+}
+
+extension AddCityViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return touch.view == self.view
     }
 }
