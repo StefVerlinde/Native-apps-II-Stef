73acf6af47064cf2187b5783a44b97e5cd18d1da
Stef Verlinde <stef.verlinde@student.hogent.be>
2020-08-16 15:54:08 +0200

    Showing the error returned by open API instead of custom error

--- WeatherStift/manager/WeatherManager.swift
+++ WeatherStift/manager/WeatherManager.swift
@@ -13,6 +13,7 @@ import Alamofire
 enum WeatherError: Error, LocalizedError {
     case unknown
     case invalidCity
+    case custom(description: String)
     
     var errorDescription: String? {
         switch self {
@@ -20,6 +21,8 @@ enum WeatherError: Error, LocalizedError {
             return "Unknown error"
         case .invalidCity:
             return "Invalid city. Please try again!"
+        case .custom(let description):
+            return description
         }
     }
 }
@@ -33,19 +36,30 @@ struct WeatherManager {
         let path = "https://api.openweathermap.org/data/2.5/weather?q=%@&appid=%@&units=metric"
         let urlString = String(format: path, query, API_KEY)
         
-        AF.request(urlString).responseDecodable(of: WeatherData.self, queue: .main, decoder: JSONDecoder()) { (response) in
+        //Validate is so we can use error.responseCode and not response.response?.statusCode
+        AF.request(urlString).validate().responseDecodable(of: WeatherData.self, queue: .main, decoder: JSONDecoder()) { (response) in
             switch response.result {
             case .success(let weatherData):
                 let model = weatherData.model
                 completion(.success(model))
             case .failure(let error):
-                if response.response?.statusCode == 404 {
-                    let invalidCityError = WeatherError.invalidCity
-                    completion(.failure(invalidCityError))
+                if let err = self.getWeatherError(error: error, data: response.data) {
+                    completion(.failure(err))
                 } else {
                     completion(.failure(error))
                 }
             }
+        }
+    }
+    
+    private func getWeatherError(error: AFError, data: Data?) -> Error? {
+        if error.responseCode == 404,
+            let data = data,
+            let failure = try? JSONDecoder().decode(WeatherDataFailure.self, from: data) {
+            let message = failure.message
+            return WeatherError.custom(description: message)
+        } else {
+            return nil
         }
     }
 }
---
+++ WeatherStift/model/WeatherDataFailure.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherDataFailure.swift
+//  WeatherStift
+//
+//  Created by Stef Verlinde on 15/08/2020.
+//  Copyright © 2020 Stef Verlinde. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherDataFailure: Decodable {
+    let message: String
+}
